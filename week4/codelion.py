c951c66170fde17487d32601e16b603c730b7487
zui0202 <zui0202@naver.com>
2022-04-29 20:34:20 +0900

    Chapter 2-8

--- week4/codelion.py
+++ week4/codelion.py
@@ -1,7 +1,13 @@
-information = {"고향":"수원", "취미":"영화관람", "좋아하는 음식":"국수"}
-print(information)
-print(information.get("취미"))
+for x in range(30):
+    print(x)
 
-information = {"특기":"피아노", "사는곳":"서울"}
-print(information.get("특기"))
-print(information.get("사는곳"))
+foods = ["된장찌개", "피자", "제육볶음"]
+for x in range(3):
+    print(foods[x])
+for x in foods:
+    print(x)
+
+information = {"고향":"수원", "취미":"영화관람", "좋아하는 음식":"국수"}
+for x, y in information.items():
+    print(x)
+    print(y)
