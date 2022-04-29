34adc7f1fbcf2773464a57b5ee5e8ae357507ba0
zui0202 <zui0202@naver.com>
2022-04-29 20:38:38 +0900

    Chapter 2-9

--- week4/codelion.py
+++ week4/codelion.py
@@ -1,13 +1,5 @@
-for x in range(30):
-    print(x)
-
 foods = ["된장찌개", "피자", "제육볶음"]
-for x in range(3):
-    print(foods[x])
-for x in foods:
-    print(x)
-
-information = {"고향":"수원", "취미":"영화관람", "좋아하는 음식":"국수"}
-for x, y in information.items():
-    print(x)
-    print(y)
+foods_set1 = set(foods)
+foods_set2 = set(["된장찌개", "피자", "제육볶음"])
+print(foods_set1)
+print(foods_set2)
