7ce28e08407a63475ee246abd1fc69f567a0c97e
zui0202 <zui0202@naver.com>
2022-04-29 21:03:23 +0900

    오늘은 뭐먹지?

--- week4/codelion.py
+++ week4/codelion.py
@@ -1,10 +1,12 @@
-import random
+lunch = ["된장찌개", "피자", "제육볶음", "짜장면"]
+set_lunch = set(lunch)
 
-food = random.choice(["된장찌개","피자","제육볶음"])
-
-print(food)
-if(food == "제육볶음"):
-    print("곱배기 주세요")
-else:
-    print("그냥 주세요")
-print("종료")
+while True:
+    print(lunch)
+    item = input("음식을 추가 해주세요 : ")
+    lunch.append(item)
+    if(item == "q"):
+        break
+    else:
+        lunch.append(item)
+print(lunch)
