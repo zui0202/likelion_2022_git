55e1a806aa10b91d92c4e01ee52e531825de7607
zui0202 <zui0202@naver.com>
2022-04-29 21:45:38 +0900

    이상형이 뭐예요?

--- week4/codelion.py
+++ week4/codelion.py
@@ -1,19 +1,14 @@
-def make_dolcelatte():
-    print("1. 얼음을 넣는다.")
-    print("2. 연유를 30ml 넣는다.")
-    print("3. 찬 우유를 넣는다.")
-    print("4. 에스프레소샷을 넣는다.")
+total_list = []
 
-def make_blueberry_smoothie():
-    print("1. 블루베리 20g을 넣는다.")
-    print("2. 우유 300ml를 넣는다.")
-    print("3. 얼음을 넣는다.")
-    print("4. 믹서기에 간다.")
+while True:
+    question = input("질문을 입력해주세요 : ")
+    if question == "q":
+        break
+    else:
+        total_list.append({"질문" : question, "답변" : ""})
 
-def make_simple_latte():
-    print("1. 커피를 넣는다.")
-    print("2. 우유를 넣는다.")
-    print("3. 신나게 섞는다.")
-
-make_dolcelatte()
-make_dolcelatte()
+for i in total_list:
+    print(i["질문"])
+    answer = input("답변을 입력해주세요 : ")
+    i["답변"] = answer
+print(total_list)
