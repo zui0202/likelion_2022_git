60b30eb83aa1e9a9038d0a2548db4fd44fe25357
zui0202 <zui0202@naver.com>
2022-04-29 20:08:41 +0900

    Chapter 2-4

--- week4/codelion.py
+++ week4/codelion.py
@@ -1,3 +1,8 @@
 import random
+import time
 
-print(random.choice(["된장찌개", "피자", "제육볶음"]))
+while True:
+    print(random.choice(["된장찌개","치킨","떡볶이","라면","감자튀김"]))
+    break
+    print("이 문장도 반복되나")
+    time.sleep(1)
