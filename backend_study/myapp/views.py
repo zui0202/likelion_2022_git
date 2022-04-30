215ed240a061d2a40f7c8e365da7b17d4317c484
zui0202 <zui0202@naver.com>
2022-04-30 19:41:56 +0900

    benefit of django

--- backend_study/myapp/views.py
+++ backend_study/myapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome!')
+    return HttpResponse('<h1>Random</h1>'+str(random.random()))
 
 def create(request):
     return HttpResponse('Create')
