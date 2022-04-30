ba5ec41035f889ac1a03e5108714b97fa40926d3
zui0202 <zui0202@naver.com>
2022-04-30 20:06:43 +0900

    read

--- backend_study/myapp/views.py
+++ backend_study/myapp/views.py
@@ -2,12 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
+topics = [{'id':1, 'title':'routing', 'body':'Routing is..'},
+{'id':2, 'title':'view', 'body':'View is..'},
+{'id':3, 'title':'model', 'body':'Model is..'},]
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ol>
+            {ol}
+        </ol>  
+        {articleTag}
+    </body>
+    </html>
+    '''
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Random</h1>'+str(random.random()))
+    article ='''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def create(request):
     return HttpResponse('Create')
 
 def read(request, id):
-    return HttpResponse('Read'+id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
