20162411f10df781733e515394bc6a3afd039fe9
zui0202 <zui0202@naver.com>
2022-05-07 20:55:10 +0900

    week5

--- week5/codelion.py
+++ week5/codelion.py
@@ -2,12 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 
-url = "http://www.daum.net/"
-response = requests.get(url)
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+url = "https://datalab.naver.com/keyword/realtimeList.naver?age=20s"
+response = requests.get(url,headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 rank = 1
+# span - item_title
+results = soup.findAll('span','item_title')
 
-results = soup.findAll('a','link_favorsch')
+print(response.text)
 
 search_rank_file = open("rankresult.txt","a")
 
