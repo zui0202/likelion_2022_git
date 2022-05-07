203baf8d544cd39e66cb0d3c792161b066e382c1
zui0202 <zui0202@naver.com>
2022-05-07 18:13:38 +0900

    week5

--- backend_study/myapp/views.py
+++ backend_study/myapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import random
 
+nextId = 4
 topics = [{'id':1, 'title':'routing', 'body':'Routing is..'},
 {'id':2, 'title':'view', 'body':'View is..'},
 {'id':3, 'title':'model', 'body':'Model is..'},]
@@ -19,6 +21,9 @@ def HTMLTemplate(articleTag):
             {ol}
         </ol>  
         {articleTag}
+        <ul>
+            <li><a href="/create/">Create</a></li>
+        </ul>
     </body>
     </html>
     '''
@@ -31,9 +36,6 @@ def index(request):
     '''
     return HttpResponse(HTMLTemplate(article))
 
-def create(request):
-    return HttpResponse('Create')
-
 def read(request, id):
     global topics
     article = ''
@@ -41,3 +43,25 @@ def read(request, id):
         if topic['id'] == int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
     return HttpResponse(HTMLTemplate(article))
+
+@csrf_exempt
+def create(request):
+    global nextId
+    if request.method == 'GET':
+        article = '''
+            <form action="/create/" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id":nextId, "title": title, "body":body}
+        topics.append(newTopic)
+        url = '/read/'+str(nextId)
+        nextId = nextId + 1
+        return redirect(url)
+    
