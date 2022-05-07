9e83837062d9c2ac95f8b92a04b4445fcd2fc955
zui0202 <zui0202@naver.com>
2022-05-07 21:00:47 +0900

    week5

--- week5/codelion.py
+++ week5/codelion.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 city = "Seoul"
 apikey = "################################"
@@ -6,3 +7,8 @@ api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
 
 result = requests.get(api)
 print(result.text)
+
+data = json.loads(result.text)
+
+print(type(result.text))
+print(type(data))
