31f8b14e0be2b52421869fee60514ca0b3f812b2
zui0202 <zui0202@naver.com>
2022-05-07 21:17:36 +0900

    week5

--- week5/codelion.py
+++ week5/codelion.py
@@ -1,7 +1,24 @@
 import smtplib
+from email.message import EmailMessage
+import imghdr
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
 
+message = EmailMessage()
+message.set_content("코드라이언 수업중입니다.")
+
+message["Subject"] = "이것은 제목입니다."
+message["From"] = "###@gmail.com"
+message["To"] = "###@gmail.com"
+
+with open("codelion.png","rb") as image:
+    image_file = image.read()
+
+image_type = imghdr.what('codelion',image_file)
+message.add_attachment(image_file,maintype='image',subtype=image_type)
+
 smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
-print(smtp)
+smtp.login("###@gmail.com","######")
+smtp.send_message(message)
+smtp.quit()
