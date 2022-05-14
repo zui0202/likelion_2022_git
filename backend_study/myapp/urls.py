b26ed184988ac023af6d97c59ca41b0d7318d633
zui0202 <zui0202@naver.com>
2022-05-14 21:28:52 +0900

    update

--- backend_study/myapp/urls.py
+++ backend_study/myapp/urls.py
@@ -5,5 +5,7 @@ from myapp import views
 urlpatterns = [
     path('', views.index),
     path('create/', views.create),
-    path('read/<id>/', views.read), 
+    path('read/<id>/', views.read),
+    path('delete/', views.delete),
+    path('update/<id>/', views.update), 
 ]
--- backend_study/myapp/views.py
+++ backend_study/myapp/views.py
@@ -8,8 +8,19 @@ topics = [{'id':1, 'title':'routing', 'body':'Routing is..'},
 {'id':2, 'title':'view', 'body':'View is..'},
 {'id':3, 'title':'model', 'body':'Model is..'},]
 
-def HTMLTemplate(articleTag):
+def HTMLTemplate(articleTag, id=None):
     global topics
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+           <li>
+                <form action="/delete/" method="post">
+                    <input type="hidden" name="id" value={id}>
+                    <input type="submit" value="delete">
+                </form>
+            </li> 
+            <li><a href="/update/{id}">update</a></li>
+        '''
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
@@ -23,6 +34,7 @@ def HTMLTemplate(articleTag):
         {articleTag}
         <ul>
             <li><a href="/create/">Create</a></li>
+            {contextUI}
         </ul>
     </body>
     </html>
@@ -42,7 +54,7 @@ def read(request, id):
     for topic in topics:
         if topic['id'] == int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
-    return HttpResponse(HTMLTemplate(article))
+    return HttpResponse(HTMLTemplate(article, id))
 
 @csrf_exempt
 def create(request):
@@ -65,3 +77,41 @@ def create(request):
         nextId = nextId + 1
         return redirect(url)
     
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method =='POST':
+        id = request.POST['id']
+        newTopics = []
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+        topics = newTopics
+        return redirect('/')
+    
+@csrf_exempt
+def update(request, id):
+    global topics
+    if request.method == 'GET':
+        for topic in topics:
+            if topic['id'] == int(id):
+                selectedTopic = {
+                    "title": topic['title'],
+                    "body": topic['body']
+                }
+        article = f'''
+            <form action="/update/{id}/" method="post">
+                <p><input type="text" name="title" placeholder="title" value={selectedTopic["title"]}></p>
+                <p><textarea name="body" placeholder="body">{selectedTopic['body']}</textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article, id))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body'] 
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title'] = title
+                topic['body'] = body
+        return redirect(f'/read/{id}')
